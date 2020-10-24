36c652fb11ea86a15c3ebe47dff2242b5286718c
Kien Phan <kien@iwa.fi>
2020-10-24 10:48:27 +0700

    Add basic bootstrap form for phones

--- Gemfile
+++ Gemfile
@@ -62,6 +62,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
+gem 'bootstrap-sass', '~> 3.4.1'
 gem 'devise', '~> 4.7'
 gem 'discard', '~> 1.2'
 gem 'rails-i18n', '~> 6.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+manufacturers = %w[Apple Samsung Nokia Vin]
+manufacturers.each do |manu|
+  Manufacturer.find_or_create_by(name: manu)
+end
+
+manufacturer = Manufacturer.find_by(name: 'Apple')
+models = [
+  {
+    name: 'iPhone X',
+    manufacturer: manufacturer,
+    os_version: 'iOS 9',
+    year_of_manufacture: 2018,
+  },
+  {
+    name: 'iPhone 11',
+    manufacturer: manufacturer,
+    os_version: 'iOS 12',
+    year_of_manufacture: 2019,
+  },
+  {
+    name: 'iPhone 12',
+    manufacturer: manufacturer,
+    os_version: 'iOS 12',
+    year_of_manufacture: 2020,
+  },
+]
+models.each do |model|
+  Model.create(model)
+end
+
+10.times do
+  Phone.create(
+    body_color: %w[pink yellow white black].sample,
+    data_memory: [64, 128, 256].sample,
+    model: Model.take,
+    price: [699, 799, 899, 999].sample,
+  )
+end
